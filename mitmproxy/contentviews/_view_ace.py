64651698a0a78e731ae45af4a652f507806b66cf
Yoav Magid <yoav@lumia.security>
2025-07-28 12:44:39 -0400

    Fixes after CR

--- mitmproxy/contentviews/_view_ace.py
+++ mitmproxy/contentviews/_view_ace.py
@@ -1,15 +1,13 @@
-import json
 import base64
-import zlib
+import json
 import pprint
-
+import zlib
 from enum import Enum
 
 import biplist
 from blackboxprotobuf import decode_message
 
-from mitmproxy.contentviews._api import Contentview
-from mitmproxy.contentviews._api import Metadata
+from mitmproxy.contentviews._api import Contentview, Metadata
 
 
 class MagicEnum(Enum):
@@ -22,12 +20,13 @@ class ACERecording:
         self.frames = frames
     
     @staticmethod
-    def _resolve_provide_context(frame):
-        if 'context' in frame:
-            frame['context'] = json.loads(frame['context'])
-            if 'conversationStateAttachments' in frame['context']:
+    def _resolve_context(frame):
+        result = frame.copy()
+        if 'context' in result:
+            result['context'] = json.loads(result['context'])
+            if 'conversationStateAttachments' in result['context']:
                 attachments = []
-                for attachment in frame['context']['conversationStateAttachments']:
+                for attachment in result['context']['conversationStateAttachments']:
                     if 'base64EncodedIntent' in attachment:
                         attachment['base64EncodedIntent'] = decode_message(base64.b64decode(attachment['base64EncodedIntent']))
                     if 'stepResults' in attachment:
@@ -35,24 +34,8 @@ class ACERecording:
                             if 'base64EncodedProtobufMessage' in stepResult:
                                 stepResult['base64EncodedProtobufMessage'] = decode_message(base64.b64decode(stepResult['base64EncodedProtobufMessage']))
                     attachments.append(attachment)
-                frame['context']['conversationStateAttachments'] = attachments
-        return frame
-    
-    @staticmethod
-    def _resolve_spoken_notification(frame):
-        if 'context' in frame:
-            frame['context'] = json.loads(frame['context'])
-            if 'conversationStateAttachments' in frame['context']:
-                attachments = []
-                for attachment in frame['context']['conversationStateAttachments']:
-                    if 'base64EncodedIntent' in attachment:
-                        attachment['base64EncodedIntent'] = decode_message(base64.b64decode(attachment['base64EncodedIntent']))
-                    if 'stepResults' in attachment:
-                        for stepResult in attachment['stepResults']:
-                            if 'base64EncodedProtobufMessage' in stepResult:
-                                stepResult['base64EncodedProtobufMessage'] = decode_message(base64.b64decode(stepResult['base64EncodedProtobufMessage']))
-                    attachments.append(attachment)
-                frame['context']['conversationStateAttachments'] = attachments
+                result['context']['conversationStateAttachments'] = attachments
+        return result
 
     @classmethod
     def parse(cls, content):
@@ -64,11 +47,11 @@ class ACERecording:
                 size = int.from_bytes(content[index+3:index+5], byteorder='big')
                 frame = content[index+5:index+5+size]
                 parsed_frame = biplist.readPlistFromString(frame)
-                if '$class' in parsed_frame and parsed_frame['$class'] == 'ProvideContext':
-                    cls._resolve_provide_context(parsed_frame)
-                elif '$class' in parsed_frame and parsed_frame['$class'] == 'SpokenNotificationProvideContext':
-                    cls._resolve_spoken_notification(parsed_frame)
-                results.append(parsed_frame)
+                if '$class' in parsed_frame and parsed_frame['$class'] in ('ProvideContext', 'SpokenNotificationProvideContext'):
+                    resolved_frame = cls._resolve_context(parsed_frame)
+                    results.append(resolved_frame)
+                else:
+                    results.append(parsed_frame)
                 index += 5 + size
             elif signature.hex() == MagicEnum.SEQUENCE_NUMBER.value:
                 sequence_number = int.from_bytes(content[index+3:index+5], byteorder='big')
@@ -95,29 +78,12 @@ class ACEContentview(Contentview):
     syntax_highlight = "yaml"
 
     def prettify(self, data: bytes, metadata: Metadata) -> str:
-        # data = json.loads(data)
-        # return json.dumps(data, indent=4, ensure_ascii=False)
-        # return "ACE"
         return pprint.pformat(ACERecording.parse_ace(data).frames)
 
     def render_priority(self, data: bytes, metadata: Metadata) -> float:
         if metadata.http_message.method == "ACE":
             return 1
         return 0
-        # if not data:
-        #     return 0
-        # if metadata.content_type in (
-        #     "application/json",
-        #     "application/json-rpc",
-        # ):
-        #     return 1
-        # if (
-        #     metadata.content_type
-        #     and metadata.content_type.startswith("application/")
-        #     and metadata.content_type.endswith("json")
-        # ):
-        #     return 1
-        # return 0
 
 
 ace_view = ACEContentview()
